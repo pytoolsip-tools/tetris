f8a2f048ceb34bf7bbc29d3d603cbfdc93bdcc16
JimDreamHeart <15602291936@163.com>
2019-05-18 19:30:11 +0800

    refactor:修改工程配置

--- assets/ProjectConfig.py
+++ assets/ProjectConfig.py
@@ -5,8 +5,8 @@
 # @Last Modified time: 2019-03-18 21:32:24
 
 ProjectConfig = {
-	"name" : "工具开发模板", # 工具名称
-	"version" : "1.0.1", # 工具版本
+	"name" : "俄罗斯方块", # 工具名称
+	"version" : "1.0.0", # 工具版本
 	"winSize" : (960,640), # 模板窗口默认大小
 	"isOpenLogWin" : True, # 是否打开日志窗口
 }
